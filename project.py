ec837573f06a33bdb94ce157411cf38e5a0fa891
n-smola-n <nastaliiasmoleva2@yandex.ru>
2022-12-18 23:05:11 +0300

    добавлены классы для сундуков, брони и хлеба + связка с MainHero, теперь все объекты - спрайты, осталось только поле заспрайтовать. Картинка героя меняется в зависимости от направления, (Диана, добавь пж их под видом: спина - MH_go0.png, а лицо - MH_go2.png в папке data) вообще было добавлено много чего)

--- project.py
+++ project.py
@@ -2,7 +2,6 @@ import pygame
 import sys
 import os
 
-
 TILES_SIZE = 50
 
 
@@ -39,10 +38,6 @@ class BaseCharacter:
     def get_cords(self):
         return self.pos_x, self.pos_y
 
-    def render(self, screen):
-        center = self.pos_x * TILES_SIZE + TILES_SIZE // 2, self.pos_y * TILES_SIZE + TILES_SIZE // 2
-        pygame.draw.circle(screen, (255, 0, 0), center, TILES_SIZE // 2)
-
 
 class BaseEnemy(BaseCharacter):
     def __init__(self, pos_x, pos_y, weapon, hp):
@@ -60,40 +55,80 @@ class MainHero(BaseCharacter, pygame.sprite.Sprite):
     image = load_image("data\\MH_stay.png")
 
     def __init__(self, pos_x, pos_y, hp, name):
-        BaseCharacter.__init__(pos_x, pos_y, hp)
-        pygame.sprite.Sprite.__init__(all_sprites)
+        BaseCharacter.__init__(self, pos_x, pos_y, hp)
+        pygame.sprite.Sprite.__init__(self)
 
-        self.image = MainHero.image
+        self.armor = []
+        self.orientation = 1
+        self.image = self.image_change()
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
         self.rect.x = pos_x
         self.rect.y = pos_y
 
         self.name = name
-        self.weapon = None
-        self.Wlist = []
+        self.bread = 0
+        self.weapon = Weapon('Кулаки', 10)
+        self.defence = 0
 
     def hit(self, target):
-        if not self.weapon:
-            print('Я безоружен')
-            return
-        else:
-            self.weapon.hit(target)
-            return
+        self.weapon.hit(target)
+        return
 
     def add_weapon(self, weapon):
-        if type(weapon) == Weapon:
-            if not self.Wlist:
-                self.weapon = weapon
-            self.Wlist.append(weapon)
-            print(f'Подобрал {weapon}')
-            return
-        print('Это не оружие')
+        self.weapon = weapon
+        print(f'Подобрал {weapon}')
+        return
+
+    def add_bread(self):
+        self.bread += 1
+
+    def add_armor(self, armor):
+        self.armor.append(armor)
+        self.defence += armor.defence
 
     def heal(self, amount):
         self.hp += amount
         self.hp = self.hp % 200 if self.hp != 200 else 200
         print('Полечился, теперь здоровья', self.hp)
+
+    def check_chest(self, hero, chest):
+        if chest.stuff:
+            for i in chest.stuff:
+                if i == 'Bread':
+                    hero.add_bread(i)
+                elif type(i) == Weapon:
+                    hero.add_weapon(i)
+                elif type(i) == Armor:
+                    hero.add_armor(i)
+            chest.stuff = []
+
+    def set_position(self, x, y, k):
+        self.rect.x = x
+        self.rect.y = y
+        if not pygame.sprite.spritecollideany(self, borders):
+            if self.orientation != k and k != -1:
+                self.orientation = k
+                self.image = self.image_change()
+            self.rect.x = x
+            self.rect.y = y
+            self.pos_x = x
+            self.pos_y = y
+            return
+        self.rect.x = self.pos_x
+        self.rect.y = self.pos_y
+
+    def image_change(self):
+        if self.orientation == 1:
+            im = load_image('data\\MH_go.png')
+        elif self.orientation == 3:
+            im = load_image('data\\MH_go.png')
+            im = pygame.transform.flip(im, flip_x=True, flip_y=False)
+        elif self.orientation == 0:
+            im = load_image(f'data\\MH_go{0}.png')
+        elif self.orientation == 2:
+            im = load_image(f'data\\MH_go{2}.png')
+        return pygame.transform.scale(im, (70, 180))
 
 
 class Weapon:
@@ -106,6 +141,26 @@ class Weapon:
     def hit(self, target):
         target.get_damage(self.damage)
 
+    def render(self):
+        pass
+
+
+class Armor:
+    def __init__(self, name, defence):
+        self.name = name
+        self.defence = defence
+
+    def render(self):
+        pass
+
+
+class Chest:
+    def __init__(self, *stuff):
+        self.stuff = list(stuff)
+
+    def render(self):
+        pass
+
 
 class Border(pygame.sprite.Sprite):
     # строго вертикальный или строго горизонтальный отрезок
@@ -114,10 +169,12 @@ class Border(pygame.sprite.Sprite):
         if x1 == x2:  # вертикальная стенка
             self.add(borders)
             self.image = pygame.Surface([1, y2 - y1])
+            self.image.fill((255, 0, 0))
             self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
         else:  # горизонтальная стенка
             self.add(borders)
             self.image = pygame.Surface([x2 - x1, 1])
+            self.image.fill((255, 0, 0))
             self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
 
 
@@ -136,7 +193,7 @@ class Board:
         self.left = 0
         self.top = 0
         self.cell_size = 20
-        self.colors = {'0': (0, 0, 0), '1': (255, 255, 255), '2': (0, 255, 0)}
+        self.colors = {'0': (0, 0, 0), '1': (255, 255, 255), '2': (0, 255, 0), '3': (200, 200, 0)}
 
     # настройка внешнего вида
     def set_view(self, left, top, cell_size):
@@ -184,18 +241,34 @@ class Game:
     def __init__(self, board, hero):
         self.board = board
         self.hero = hero
+        self.k = 1
 
     def update_hero(self):
         next_x, next_y = self.hero.get_cords()
-        if pygame.key.get_pressed()[pygame.K_LEFT]:
-            next_x -= 1
-        if pygame.key.get_pressed()[pygame.K_RIGHT]:
-            next_x += 1
-        if pygame.key.get_pressed()[pygame.K_UP]:
-            next_y -= 1
-        if pygame.key.get_pressed()[pygame.K_DOWN]:
-            next_y += 1
-        self.hero.set_position(next_x, next_y)
+        '''
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+            next_x -= 10
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+            next_x += 10
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+            next_y -= 10
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+            next_y += 10
+            '''
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            next_x -= 10
+            self.k = 3
+        elif keys[pygame.K_RIGHT]:
+            next_x += 10
+            self.k = 1
+        elif keys[pygame.K_UP]:
+            next_y -= 10
+            self.k = 0
+        elif keys[pygame.K_DOWN]:
+            next_y += 10
+            self.k = 2
+        self.hero.set_position(next_x, next_y, self.k)
 
 
 all_sprites = pygame.sprite.Group()
@@ -204,32 +277,38 @@ borders = pygame.sprite.Group()
 
 def main():
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-    screen.fill((255, 255, 255))
     board = Board(33, 18, 'map1.txt')
     board.set_view(100, 100, TILES_SIZE)
-    hero = MainHero(10, 10, 50, 'chara')
+    hero = MainHero(300, 300, 50, 'chara')
+    all_sprites.add(hero)
     game = Game(board, hero)
-    Border(board.left, board.top, board.left, TILES_SIZE * board.height)
-    Border(board.left, board.top, TILES_SIZE * board.width, board.top)
-    #Border(board.left, board.top, board.left, TILES_SIZE * board.height)
-    #Border(board.left, board.top, board.left, TILES_SIZE * board.height)
+    Border(board.left, board.top,
+           board.left, TILES_SIZE * board.height + board.top)
+    Border(board.left, board.top,
+           TILES_SIZE * board.width + board.left, board.top)
+    Border(board.left + TILES_SIZE * board.width, board.top,
+           board.left + TILES_SIZE * board.width, TILES_SIZE * board.height + board.top)
+    Border(board.left, board.top + TILES_SIZE * board.height,
+           board.left + TILES_SIZE * board.width, board.top)
 
     running = True
     clock = pygame.time.Clock()
+    i = 0
     while running:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 board.get_click(event.pos)
-            game.update_hero()
+        game.update_hero()
 
         board.render(screen)
-        hero.render(screen)
-        all_sprites.draw(screen)
+
         all_sprites.update()
+        all_sprites.draw(screen)
+
         pygame.display.flip()
-        clock.tick(60)
+        clock.tick(20)
 
 
 if __name__ == '__main__':
